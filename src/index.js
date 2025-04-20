9aa04c009f869c56e86741063f16afaa91f5680f
ashpal <164777081+jdmsquash@users.noreply.github.com>
2025-04-20 10:11:12 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    
-    // Serve index.html for the root path
-    if (url.pathname === "/" || url.pathname === "") {
-      return new Response(await env.ASSETS.fetch(new Request("/index.html")).then(res => res.text()), {
-        headers: { "Content-Type": "text/html;charset=UTF-8" }
-      });
-    }
-    
-    // Try to serve the requested asset
-    try {
-      return await env.ASSETS.fetch(request);
-    } catch (e) {
-      // If asset not found, serve index.html
-      return new Response(await env.ASSETS.fetch(new Request("/index.html")).then(res => res.text()), {
-        headers: { "Content-Type": "text/html;charset=UTF-8" }
-      });
-    }
+  async fetch(request, env, ctx) {
+    // Return the static assets from the "public" directory
+    return env.ASSETS.fetch(request);
   }
-}
+};
